c7ce47edc6d9ba7221aa42eab17782fa5b359c50
Felix <hola@flopezluis.es>
2016-01-25 11:06:11 +0100

    Fixed incorrect tests

--- test/kvstore/core_test.clj
+++ test/kvstore/core_test.clj
@@ -10,19 +10,19 @@
     (let [s (s/stream)]
       (store/put! "key" "value")
       (core/get-value "key" s)
-      (is (= "value") @(s/take! s)))))
+      (is (= "value\r\n" @(s/take! s))))))
 
 (deftest test-write-value
   (testing "testing a command is well parsed."
     (let [s (s/stream)]
       (core/write-operation "key2" "value2" s)
-      (is (= "OK\r\n") @(s/take! s))
+      (is (= "OK\r\n" @(s/take! s)))
       (core/get-value "key2" s)
-      (is (= "value2") @(s/take! s)))))
+      (is (= "value2\r\n" @(s/take! s))))))
 
 (deftest test-fwrite-value
   (testing "testing a command is well parsed."
     (let [s (s/stream)]
       (core/fwrite-operation "key3" "value3" s)
       (core/get-value "key3" s)
-      (is "value3" @(s/take! s)))))
+      (is (= "value3\r\n" @(s/take! s))))))
--- test/kvstore/protocol_test.clj
+++ test/kvstore/protocol_test.clj
@@ -2,22 +2,36 @@
   (:require [clojure.test :refer :all]
             [kvstore.protocol :refer :all]
             [manifold.stream :as s]
-            [kvstore.protocol :as protocol]
-            [kvstore.store :as store]))
+            [kvstore.protocol :as protocol :refer [parse-cmd]]
+            [kvstore.store :as store])
+  (:import [java.util.concurrent ArrayBlockingQueue]))
 
-(deftest test-process-set-cmd
-  (testing "testing a command is well parsed."
-    (let [s (s/stream)]
-      (is (= "OK") (protocol/run-cmd "SET b 1" s))
-      (is (= "1") (get #'store/storage "b")))))
+(defn cmd1 [arg0 arg1 stream]
+  (is (= arg0 "arg0"))
+  (is (= arg1 "arg1"))
+  (s/put! stream "CMD1\r\n"))
 
-(deftest test-process-get-cmd
-  (testing "testing a command is well parsed."
-    (let [s (s/stream)]
-      (is (= "OK") (protocol/run-cmd "SET b 1" s))
-      (is (= "1") (store/get-key "b")))))
+(defn cmd2 [arg0 stream]
+  (is (= arg0 "arg0"))
+  (s/put! stream "CMD2\r\n"))
 
-(deftest test-process-close-cmd
+(defmethod parse-cmd "CMD1"
+  [cmd & args]
+  (apply cmd1 args))
+
+(defmethod parse-cmd "CMD2"
+  [cmd & args]
+  (apply cmd2 args))
+
+(deftest test-run-cmd
   (testing "testing a command is well parsed."
-    (let [s (s/stream)]
-      (is (= ::close) (protocol/run-cmd "CLOSE" s)))))
+    (protocol/run-cmd "CMD1 arg0 arg1" (s/stream))))
+
+(deftest test-process-commands []
+  (testing "testing the stream of commands is well parsed."
+    (let [stream (s/stream)]
+      (protocol/process-commands stream "CMD1 arg0 arg1\r\nCMD2 arg0\r\n"
+                                 (fn [s cmd]
+                                   (run-cmd cmd s)))
+      (is (= "CMD1\r\n" @(s/take! stream)))
+      (is (= "CMD2\r\n" @(s/take! stream))))))
